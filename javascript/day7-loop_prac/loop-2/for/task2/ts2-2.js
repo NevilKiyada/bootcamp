5d1504104408fab43b0aa3a69a438baeb6590c26
NevilKiyada <nevilkiyada04@gmail.com>
2024-11-29 19:22:02 +0530

    fix some issues

--- javascript/day7-loop_prac/loop-2/for/task2/ts2-2.js
+++ javascript/day7-loop_prac/loop-2/for/task2/ts2-2.js
@@ -1,8 +1,9 @@
 for(var i=1; i<=4; i++)
 {
+    let str = "";
     for(var j=1; j<=i; j++)
     {
-        console.log("*");
+        str += "*";
     }
-    console.log();
+    console.log(str);
 }
