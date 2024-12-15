d5a52a34c4db4441d3a0bbc8ae8011ea7f71e3b5
NevilKiyada <nevilkiyada04@gmail.com>
2024-12-15 12:37:30 +0530

    calculator complit with eval function

---
+++ javascript/day11-dom/task/calculator/script.js
@@ -0,0 +1,72 @@
+let values = "";  // Tracks the current number being entered
+let ans = "";     // Builds the full input string
+let operator = ""; // Tracks the current operator (optional)
+
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const className = event.target.className;
+    const buttonText = button.innerText;
+
+    if (className === "button") {
+
+      values += buttonText;
+      document.querySelector(".display").innerText = values;
+
+
+    } else if (className.includes("operator")) {
+      document.querySelector(".display").innerText = "0";
+    
+      if (values != "") {
+        operator = buttonText
+        ans += values;
+        values = ""
+      }
+      else{
+        ans += buttonText
+      }
+      
+
+      ans += operator;
+      document.querySelector(".display").innerText = ans;
+
+    } else if (className.includes("equals")) {
+      ans += values;
+      console.log(ans);
+      let result  = "0"
+      if (ans != "") {
+        result = eval(ans.replace("×", "*").replace("÷", "/"));
+        console.log(result);
+        values = ""
+        operator = ""
+        document.querySelector(".display").innerText = result;
+      }
+
+      document.querySelector(".display").innerText = result;
+
+
+
+    } else if (className.includes("clear")) {
+
+      ans = ""
+      values = ""
+      operator = ""
+      document.querySelector(".display").innerText = 0;
+    }
+    else if(className.includes("back")) {
+      if (values != "") {
+        operator = buttonText
+        ans += values;
+        values = ""
+      }
+      ans=ans.slice(0,-1)
+      document.querySelector(".display").innerText = ans;
+      if(ans == "") {
+        document.querySelector(".display").innerText = "0";
+
+      }
+
+    }
+  });
+});
+
+
---
+++ javascript/day11-dom/task/counter/script.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+document.querySelector('#pls').addEventListener('click', add);
+document.querySelector('#min').addEventListener('click', min);
+
+
+function add() {
+    count++;
+    document.querySelector('#show').innerHTML = count;
+}
+function min() {
+    if (count <= 0) {
+        document.querySelector('#min').disabled = true;
+    }
+    else {
+        count--;
+        document.querySelector('#min').disabled = false;
+    }
+
+    document.querySelector('#show').innerHTML = count;
+}
+
