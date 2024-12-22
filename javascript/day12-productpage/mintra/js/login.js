5c3fe640795b2410f0848b5b4944836b787f3c9c
NevilKiyada <nevilkiyada04@gmail.com>
2024-12-22 19:35:18 +0530

    login is also work normal

--- javascript/day12-productpage/mintra/js/login.js
+++ javascript/day12-productpage/mintra/js/login.js
@@ -1,37 +1,43 @@
-document.getElementById("loginForm").addEventListener("submit",(a)=>{
+document.getElementById("loginForm").addEventListener("submit", (a) => {
     a.preventDefault();
 
     let email = document.getElementById("loginEmail").value;
     let pas = document.getElementById("loginPassword").value;
-
-    if (valid(email,pas)){
+    let val = valid(email, pas)
+    if (val) {
         alert("login successful")
     }
-    else{
-        alert("failed to login");
-    }
+    
 
 })
 
 
-function valid(email,password) {
+function valid(email, password) {
     fetch(`http://localhost:3000/users?email=${email}`)
-    .then((res)=>{
-        return res.json()
-    })
-    .then((Res)=>{
-        console.log(Res)
-        if(Res[0].password == password){
-            alert("login successful")
-             window.location.href = "../HTML/index.html"
+        .then((res) => {
+            return res.json()
+        })
+        .then((Res) => {
+            console.log(Res)
+            if (Res.length == 0) {
+                alert("email is not valid")
+                return false
+            }
+            else {
+                if (Res[0].password == password) {
+                    window.location.href = "http://127.0.0.1:5500/javascript/day12-productpage/mintra/html/index.html"
+                    alert("login successful")
+                    return true
 
-            // return true
-        }
-        else{
-           
-        }
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+
+                }
+                else {
+                    alert("password is not valid");
+                    return false
+                }
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
