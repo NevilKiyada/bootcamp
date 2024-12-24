f41aa2780fa734344f1c6a745b1a6fad0771842b
NevilKiyada <nevilkiyada04@gmail.com>
2024-12-24 15:54:51 +0530

    cart is working

--- javascript/day12-productpage/mintra/js/singleproduct.js
+++ javascript/day12-productpage/mintra/js/singleproduct.js
@@ -1,19 +1,19 @@
-(function (){
+(function () {
     let ur = new URLSearchParams(window.location.search);
 
-    let id =ur.get("id");
+    let id = ur.get("id");
 
-    fetch(`http://localhost:3000/products/${id}`).then((res)=>res.json()).then((prod)=>{
-       document.getElementById("box").innerHTML = View(prod)
+    fetch(`http://localhost:3000/products/${id}`).then((res) => res.json()).then((prod) => {
+        document.getElementById("box").innerHTML = View(prod)
     });
-    
+
 })()
 
 
 function View(prod) {
-    console.log(typeof(prod));
+    console.log(typeof (prod));
     // return pro.map((prod) =>{
-        return `
+    return `
                     <div>
                         <div>
                             <img src="${prod.image}" alt="${prod.title}" height="100px" width="130px">
@@ -23,19 +23,32 @@ function View(prod) {
 
                         </div>
 
-                        <button id="${prod.id}" oncick(addtocart(${prod.id})) > Cart</button>   
+                        <button id="${prod.id}" onclick=addtocart(${prod.id}) >Add To Cart</button>   
                     </div>
 
 
 
                 `
     // }).join("")
-    
+
 }
 
 
-function addtocart(id){
-    fetch(`http://localhost:3000/products/${id}`).then((res)=>res.json()).then((prod)=>{
-       View(prod)
-     });
+function addtocart(id) {
+    fetch(`http://localhost:3000/products/${id}`).then((res) => res.json()).then((prod) => {
+        add(prod)
+    });
+   
+
+}
+
+function add(prod){
+    fetch(`http://localhost:3000/cart/`,{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({data : prod , quantity: 1}),
+    }).then((res) => res.json())
+    .then((prod) => {
+        console.log(prod);
+    }).catch((err) => console.log(err));
 }
