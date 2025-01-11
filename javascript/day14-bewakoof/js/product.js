29d960d292090c1cc978a1643e82b8a4922f9a1a
NevilKiyada <nevilkiyada04@gmail.com>
2025-01-11 19:05:00 +0530

    daily work

---
+++ javascript/day14-bewakoof/js/product.js
@@ -0,0 +1,14 @@
+document.getElementsByClassName("leftscroll").innerHTML=`<div class="card" style="width: 18rem;">
+                <img class="card-img-top "
+                    src="https://images.bewakoof.com/t640/men-s-red-graphic-printed-oversized-sweatshirt-644624-1730281473-1.jpg"
+                    alt="Card image cap" height="100%" width="100%">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>`;
+
+
+console.log("hii")
